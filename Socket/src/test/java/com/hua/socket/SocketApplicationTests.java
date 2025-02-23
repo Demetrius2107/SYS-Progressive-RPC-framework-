1f41c31adb8be6cfc073501600a401497b2c4c3c
Lipschultz <2562017477@qq.com>
2025-02-23 18:48:59 +0800

    feat: add test

---
+++ Socket/src/test/java/com/hua/socket/SocketApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hua.socket;
+
+/**
+ * @author: Elon
+ * @title: SocketApplicationTests
+ * @projectName: Progressive-RPC-freamework
+ * @description: TODO
+ * @date: 2025/2/23 18:48
+ */
+
+public class SocketApplicationTests {
+}
