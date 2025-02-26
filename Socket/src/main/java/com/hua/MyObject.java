0a3e4c6165335acde17ebd4a0c84868c9a049b89
Lipschultz <2562017477@qq.com>
2025-02-26 17:46:35 +0800

    feat: service and socket protocol consummate

--- Socket/src/main/java/com/hua/MyObject.java
+++ Socket/src/main/java/com/hua/MyObject.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 /**
  * @author: Elon
  * @title: MyObject
- * @projectName: Progressive-RPC-freamework
- * @description: TODO
+ * @projectName: Progressive-RPC-framework
+ * @description:
  * @date: 2025/2/23 18:08
  */
 public class MyObject implements Serializable {
